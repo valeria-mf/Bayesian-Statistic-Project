891ee2d2838a8f98c061bd46186012c0e202f33e
valeria-iapaolo <152727256+valeria-iapaolo@users.noreply.github.com>
2023-12-04 12:06:04 +0100

    Add files via upload

--- Call_GibbsSamplerBB.r
+++ Call_GibbsSamplerBB.r
@@ -13,10 +13,11 @@ Sys.setenv(PKG_CXXFLAGS=paste0("-I", shQuote("C:/Users/ipval/AppData/Local/R/win
 Rcpp::sourceCpp("C:/Users/ipval/Downloads/GibbsSamplerBB.cpp")
 
 n=5
-A_ <- matrix(1:20, nrow = 4, ncol = 5)
-X_ <- matrix(1:25, nrow = n, ncol = 5)
+A_ <- matrix(rnorm(4*5,1,3), nrow = 4, ncol = 5)
+X_ <- matrix(rnorm(n*5,2,2), nrow = n, ncol = 5)
+
 
 # Supponendo che A_ e X_ siano oggetti R di tipo matrice o data frame
-result <- GibbsSampler_betabernoulli(alpha=-0.5, theta=0.6, sigma_x=1, sigma_a=1,
-                                     n_tilde=10, n, A_=A_, X_=X_, n_iter=20, initial_iters=0)
+result <- GibbsSampler_betabernoulli(alpha=-7, theta=16, sigma_x=2, sigma_a=3,
+                                     n_tilde=30, n, A_=A_, X_=X_, n_iter=20, initial_iters=0)
 
--- Call_GibbsSamplerIBP.r
+++ Call_GibbsSamplerIBP.r
@@ -10,9 +10,9 @@ Sys.setenv(PKG_CXXFLAGS=paste0("-I", shQuote("C:/Users/ipval/AppData/Local/R/win
 Rcpp::sourceCpp("C:/Users/ipval/Downloads/GibbsSamplerIBP.cpp")
 
 n=5
-A_ <- matrix(1:20, nrow = 4, ncol = 5)
-X_ <- matrix(1:25, nrow = n, ncol = 5)
+A_ <- matrix(rnorm(4*5,1,3), nrow = 4, ncol = 5)
+X_ <- matrix(rnorm(n*5,2,2), nrow = n, ncol = 5)
 
 
-result <- GibbsSampler_IBP(alpha=0.5,gamma=0.5, sigma_a=1, 
-                  sigma_x=1, theta=0.6, n=n, A_=A_, X_=X_, n_iter=20,initial_iters=0)
+result <- GibbsSampler_IBP(alpha=0.7,gamma=2, sigma_a=3, 
+                  sigma_x=2, theta=16, n=n, A_=A_, X_=X_, n_iter=20,initial_iters=0)
