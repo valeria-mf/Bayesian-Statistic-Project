b45bde021a748f86303a058596729fa2695a012b
valeria-iapaolo <152727256+valeria-iapaolo@users.noreply.github.com>
2023-12-20 11:09:07 +0100

    Add files via upload

--- Call_GibbsSamplerBB.r
+++ Call_GibbsSamplerBB.r
@@ -3,21 +3,40 @@ install.packages("RcppEigen")
 
 library(Rcpp)
 library(RcppEigen)
+library(MASS)
 
 #Questo è il path del file header contenuto nella cartella unzippata dell'installazione di Eigen
-Sys.setenv("PKG_CXXFLAGS"="-I\"C:\\Users\\ipval\\Downloads\\eigen-3.4.0\\eigen-3.4.0\"")
+Sys.setenv("PKG_CXXFLAGS"="-I\"C:\\Users\\ipval\\Desktop\\Progetto Bayesiana\\eigen-3.4.0\\eigen-3.4.0\"")
 
 #Questo è il path del file creato con il comando install.packages("RcppEigen")
 Sys.setenv(PKG_CXXFLAGS=paste0("-I", shQuote("C:/Users/ipval/AppData/Local/R/win-library/4.2/RcppEigen/include")))
 
-Rcpp::sourceCpp("C:/Users/ipval/Downloads/GibbsSamplerBB.cpp")
+Rcpp::sourceCpp("C:/Users/ipval/Desktop/Progetto Bayesiana/GibbsSamplerBB.cpp")
+set.seed(1234)
 
-n=5
-A_ <- matrix(rnorm(4*5,1,3), nrow = 4, ncol = 5)
-X_ <- matrix(rnorm(n*5,2,2), nrow = n, ncol = 5)
+N=10
+K=10
+D=20
+sigma_x=2
+sigma_a=3
+mean_a=1
+generateRandomMatrix <- function(K, D) {
+  randomVector <- sample(0:1, K * D, replace = TRUE)
+  randomMatrix <- matrix(randomVector, nrow = K, ncol = D)
+  return(randomMatrix)
+}
+
+Z_true <- generateRandomMatrix(N, K)
+A_ <- matrix(rnorm(K*D,mean_a,sigma_a), nrow = K, ncol = D)
+X_ <- matrix(0, nrow=N, ncol=D)
+for (i in 1:N){
+  X_[i,] <- mvrnorm(1, mu = Z_true[i,]%*%A_ , Sigma = sigma_x^2*diag(D) )
+
+  
+}
 
 
 # Supponendo che A_ e X_ siano oggetti R di tipo matrice o data frame
-result <- GibbsSampler_betabernoulli(alpha=-7, theta=16, sigma_x=2, sigma_a=3,
-                                     n_tilde=30, n, A_=A_, X_=X_, n_iter=20, initial_iters=0)
+result <- GibbsSampler_betabernoulli(alpha=-7, theta=16, sigma_x=sigma_x, sigma_a=sigma_a,
+                                     n_tilde=30, N, A_=A_, X_=X_, n_iter=20, initial_iters=1000)
 
