136b86b7bd1b161403ad08bd810951a7174f6d77
antonio-napolitano <149016047+antonio-napolitano@users.noreply.github.com>
2023-12-27 13:23:20 +0100

    Update auxiliar_functions_anto.cpp

--- auxiliar_functions_anto.cpp
+++ auxiliar_functions_anto.cpp
@@ -77,7 +77,37 @@ double poissonProbability(int k, double lambda) {
     return (exp(-lambda) * pow(lambda, k)) / tgamma(k + 1);
 }
 
-
+// Function to perform a Metropolis-Hastings step for sigma_x or sigma_a
+double metropolis_step_sigma(double current_sigma, const MatrixXd& Z, const MatrixXd& X, const MatrixXd& A, double proposal_variance, std::default_random_engine& generator, bool is_sigma_x) {
+  std::normal_distribution<double> proposal_dist(current_sigma, sqrt(proposal_variance));
+  double new_sigma = proposal_dist(generator);
+  
+  if (new_sigma <= 0) return current_sigma; // Ensure proposed sigma is positive
+  
+  double current_likelihood, new_likelihood;
+  
+  if (is_sigma_x) {
+    // Calculate likelihood with the current and new sigma_x
+    MatrixXd M = (Z.transpose() * Z + MatrixXd::Identity(Z.cols(), Z.cols()) * pow(current_sigma/sigma_a, 2)).inverse();
+    current_likelihood = calculate_likelihood(Z, X, M, current_sigma, sigma_a, Z.cols(), A.cols(), Z.rows());
+    
+    M = (Z.transpose() * Z + MatrixXd::Identity(Z.cols(), Z.cols()) * pow(new_sigma/sigma_a, 2)).inverse();
+    new_likelihood = calculate_likelihood(Z, X, M, new_sigma, sigma_a, Z.cols(), A.cols(), Z.rows());
+  } else {
+    // Calculate likelihood with the current and new sigma_a
+    // Similar to above, adjust the calculations for sigma_a
+  }
+  
+  // Compute the acceptance ratio
+  double acceptance_ratio = exp(new_likelihood - current_likelihood);
+  
+  std::uniform_real_distribution<double> uniform(0.0, 1.0);
+  if (uniform(generator) < acceptance_ratio) {
+    return new_sigma; // Accept the new value
+  } else {
+    return current_sigma; // Reject the new value
+  }
+}
 
 
 
