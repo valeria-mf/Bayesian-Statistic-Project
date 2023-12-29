e8059c15f2d7a94383adf4d56531a77907bbbb04
valeria-mf <125977863+valeria-mf@users.noreply.github.com>
2023-12-29 18:32:17 +0100

    Update GibbsSamplerIBP.cpp
    :)

--- GibbsSamplerIBP.cpp
+++ GibbsSamplerIBP.cpp
@@ -35,7 +35,7 @@ Rcpp::List GibbsSampler_IBP(const double alpha,const double gamma,const double s
     //create a vector to put the generated Z matrices:
     matrix_collection Ret;
 
-    for (Eigen::Index it=0;it<n_iter+initial_iters;++it){
+      for (Eigen::Index it=0;it<n_iter+initial_iters;++it){
 
         MatrixXd Znew;
 
@@ -74,7 +74,7 @@ Rcpp::List GibbsSampler_IBP(const double alpha,const double gamma,const double s
 
                 Z(i,count)=1;
                 M= update_M(M,Z.row(i));
-          
+
                 long double prob_xz_1=1/(pow((Z.transpose()*Z + sigma_x*sigma_x/sigma_a/sigma_a*Eigen::MatrixXd::Identity(z_cols,z_cols)).determinant(),D*0.5) );
 
 
@@ -94,8 +94,8 @@ Rcpp::List GibbsSampler_IBP(const double alpha,const double gamma,const double s
 
                 long double prob_one_temp=prob_zz*prob_xz;
                 long double prob_zero_temp=(1-prob_zz)*prob_xz0;
-                long double prob_param=prob_one_temp/(prob_one_temp+prob_zero_temp); 
-            
+                long double prob_param=prob_one_temp/(prob_one_temp+prob_zero_temp);
+
 
 
                 //sample from Bernoulli distribution:
@@ -109,51 +109,51 @@ Rcpp::List GibbsSampler_IBP(const double alpha,const double gamma,const double s
             }
 
 
-             //sample the number of new features:
+            //sample the number of new features:
 
 
-            unsigned UB=K+4 //da cambiare
+            unsigned UB= K>20? 10:5;//da cambiare
             Z.resize(n,K+UB);
-            
+
             //update Z-part1:
             Eigen::Index j = 0;
             for (; j < Znew.cols(); ++j)
                 Z.col(j) = Znew.col(j);
-            for (Eigen::Index kk = j; kk < Z.cols(); ++kk)
+            for (Eigen::Index kk = Znew.cols(); kk < Z.cols(); ++kk)
                 Z.col(kk).setZero();
-            
+
 
 
             M = (Z.transpose() * Z -
-                 Eigen::MatrixXd::Identity(n_tilde, n_tilde) * pow(sigma_x / sigma_a, 2)).inverse();
+                 Eigen::MatrixXd::Identity(Z.cols(), Z.cols()) * pow(sigma_x / sigma_a, 2)).inverse();
 
 
             double prob=tgamma(theta+alpha+n)/tgamma(theta+alpha)*tgamma(theta)/tgamma(theta+n)*gamma;
 
             Eigen::VectorXd prob_new(UB);
-            for (unsigned itt = 0; itt < n_res; ++itt) {
-                double bin_prob = poissonProbability(prob, itt);
+            for (unsigned itt = 0; itt < UB; ++itt) {
+                double poi_prob = poissonProbability(prob, itt);
                 Z(i, j+itt) = 1;
                 M = update_M(M, Z.row(i));
                 long double p_xz_1 = 1 / (pow((Z.transpose() * Z + sigma_x * sigma_x / sigma_a / sigma_a *
-                                                                   Eigen::MatrixXd::Identity(n_tilde,
-                                                                                             n_tilde)).determinant(),
+                                                                   Eigen::MatrixXd::Identity(Z.cols(),
+                                                                                             Z.cols())).determinant(),
                                               D * 0.5));
                 MatrixXd mat = X.transpose() * (Eigen::MatrixXd::Identity(n, n) - (Z * M * Z.transpose())) * X;
                 long double p_xz_2 = mat.trace() * (-1 / (2 * sigma_x * sigma_x));
 
                 double p_xz = p_xz_1 * exp(p_xz_2);
-                prob_new(itt) = bin_prob * p_xz;
+                prob_new(itt) = poi_prob * p_xz;
             }
             // Normalize posterior probabilities
-            double sum_posterior = prob_new.sum()
+            double sum_posterior = prob_new.sum();
             for (unsigned l=0;l<prob_new.size();++l) {
-                    prob_new(l) /= sum_posterior;
-                  }
-    
-           // Sample the number of new features based on posterior probabilities
-           std::discrete_distribution<int> distribution(prob_new.begin(), prob_new.end());
-           int new_feat = distribution(generator);
+                prob_new(l) /= sum_posterior;
+            }
+
+            // Sample the number of new features based on posterior probabilities
+            std::discrete_distribution<int> distribution(prob_new.begin(), prob_new.end());
+            int new_feat = distribution(generator);
 
 
             //update Z-part2:
@@ -164,13 +164,10 @@ Rcpp::List GibbsSampler_IBP(const double alpha,const double gamma,const double s
         }
 
 
+    if(it>=initial_iters){
+
+        Ret.push_back(eliminate_null_columns(Z).first);
     }
-
-
-        if(it>=initial_iters){
-
-            Ret.push_back(eliminate_null_columns(Z).first);
-        }
 
     }
     return Rcpp::List::create(Rcpp::Named("result") = Ret);
