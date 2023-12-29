6a9bab49a68083a80a04d37874f8949a44063e22
valeria-mf <125977863+valeria-mf@users.noreply.github.com>
2023-12-30 00:44:38 +0100

    Update auxiliar_functions.cpp

--- auxiliar_functions.cpp
+++ auxiliar_functions.cpp
@@ -182,8 +182,8 @@ MatrixXd sample2_A(const MatrixXd& Z, const MatrixXd& X, double sigma_x, double 
   MatrixXd new_A(K, D);
   for(unsigned k=0; k<K; ++k) {
     for(unsigned d=0; d<D; ++d) {
-      std::normal_distribution<double> distr(mu_posterior[d], Sigma_posterior);
-      new_A[k,d] = distr(generator); // sampling dei valori di A elemento per elemento
+      std::normal_distribution<double> distr(mu_posterior(d), Sigma_posterior);
+      new_A(k,d) = distr(generator); // sampling dei valori di A elemento per elemento
     }
   }
   return new_A;
