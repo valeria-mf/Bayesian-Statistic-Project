089e5143ff35f925d9fac47d3b68698b7d480cdd
valeria-mf <125977863+valeria-mf@users.noreply.github.com>
2023-12-30 20:06:26 +0100

    Update auxiliar_functions.h
    Two functions added (metropolis_step_sigma_a, metropolis_step_sigma_x)

--- auxiliar_functions.h
+++ auxiliar_functions.h
@@ -40,10 +40,18 @@ double binomialProbability(unsigned n_res, double prob, unsigned k);
 // Function to calculate the probability of k = certain value in poisson distribution
 double poissonProbability(int k, double lambda);
 
-double metropolis_step_sigma(double current_sigma, const MatrixXd& Z, const MatrixXd& X, const MatrixXd& A, double proposal_variance, std::default_random_engine& generator, bool is_sigma_x);
+// Metropolis-Hastings step for sigma_a:
+double metropolis_step_sigma_a(double current_sigma, const MatrixXd& Z, const MatrixXd& X,
+                               const MatrixXd& A, std::default_random_engine& generator,double sd);
+// Metropolis-Hastings step for sigma_x:
+double metropolis_step_sigma_x(double current_sigma, const MatrixXd& Z, const MatrixXd& X,
+                               const MatrixXd& A, std::default_random_engine& generator,double sd);
+
+
+//double metropolis_step_sigma(double current_sigma, const MatrixXd& Z, const MatrixXd& X, const MatrixXd& A, double proposal_variance, std::default_random_engine& generator, bool is_sigma_x);
 
 // Function to perform a Metropolis-Hastings step for sigma_x or sigma_a
-double metropolis_step_sigma(double current_sigma, const MatrixXd& Z, const MatrixXd& X, const MatrixXd& A, double proposal_variance, std::default_random_engine& generator, bool is_sigma_x,double prior_variance);
+//double metropolis_step_sigma(double current_sigma, const MatrixXd& Z, const MatrixXd& X, const MatrixXd& A, double proposal_variance, std::default_random_engine& generator, bool is_sigma_x,double prior_variance);
 
 // Function to sample A matrix
 MatrixXd sample_A(const MatrixXd& Z, const MatrixXd& X, double sigma_x, double sigma_a, std::default_random_engine& generator);
