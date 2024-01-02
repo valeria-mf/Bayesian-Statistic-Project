01916576aca371e02f9955af839ce8abc7caccaf
antonio-napolitano <149016047+antonio-napolitano@users.noreply.github.com>
2024-01-02 13:16:59 +0100

    Update auxiliar_functions.h

--- auxiliar_functions.h
+++ auxiliar_functions.h
@@ -43,9 +43,20 @@ double poissonProbability(int k, double lambda);
 // Metropolis-Hastings step for sigma_a:
 double metropolis_step_sigma_a(double current_sigma, const MatrixXd& Z, const MatrixXd& X,
                                const MatrixXd& A, std::default_random_engine& generator,double sd);
+/*
+double metropolis_step_sigma_a(double current_sigma_a, const MatrixXd& Z, const MatrixXd& X, 
+                               const MatrixXd& A, double sigma_x, double proposal_variance,
+                               std::default_random_engine& generator, double prior_variance);
+*/
+
 // Metropolis-Hastings step for sigma_x:
 double metropolis_step_sigma_x(double current_sigma, const MatrixXd& Z, const MatrixXd& X,
                                const MatrixXd& A, std::default_random_engine& generator,double sd);
+/*
+double metropolis_step_sigma_x(double current_sigma_x, const MatrixXd& Z, const MatrixXd& X, 
+                               const MatrixXd& A, double sigma_a, double proposal_variance,
+                               std::default_random_engine& generator, double prior_variance);
+*/
 
 
 //double metropolis_step_sigma(double current_sigma, const MatrixXd& Z, const MatrixXd& X, const MatrixXd& A, double proposal_variance, std::default_random_engine& generator, bool is_sigma_x);
